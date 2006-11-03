f20bb8ce11afb778648e22a78c2460989c1f1b85
andrewy <andrewy>
2006-11-03 09:05:22 +0000

    use libmagic instead of file binary

--- src/handle_event.c
+++ src/handle_event.c
@@ -1,3 +1,4 @@
+#include <magic.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,11 +8,10 @@
 
 void handle_event(struct watchnode* node, struct inotify_event* event)
 {
-	char* execute;
+	char* filename;
 	char* mimetype;
 	char* temp;
-	char* temp2;
-	FILE* pipe;
+	magic_t magic;
 
 	for (; node; node = node->next)
 	{
@@ -22,35 +22,29 @@ void handle_event(struct watchnode* node, struct inotify_event* event)
 	if (!node)
 		return;
 
-	execute = malloc(strlen(node->path) + strlen("/") + strlen(event->name) + strlen("file -ib ") + 1);
-	strcpy(execute, "file -ib ");
-	strcat(execute, node->path);
-	strcat(execute, "/");
-	strcat(execute, event->name);
+	filename = malloc(strlen(node->path) + strlen("/") + strlen(event->name) + 1);
+	strcpy(filename, node->path);
+	strcat(filename, "/");
+	strcat(filename, event->name);
 	
-	if ( (pipe = popen(execute, "r")) == NULL )
-		perror("popen");
+/* does perror work here? should we also call exit()? */
+	if ( (magic = magic_open(MAGIC_MIME)) == NULL)
+		perror("magic_open");
 
-	free(execute);
+	if (magic_load(magic, NULL) < 0)
+		perror("magic_load");
 
-	temp2 = strdup("");
-	temp = malloc(30);
-	mimetype = strdup("");
-	while (fgets(temp, 29, pipe) != NULL)
-	{
-		free(temp2);
-		temp2 = strdup(mimetype);
-		free(mimetype);
-		mimetype = malloc(strlen(temp2) + strlen(temp) + 1);
-		strcpy(mimetype, temp2);
-		strcat(mimetype, temp);
-	} 
-	free(temp);
-	free(temp2);
+	mimetype = magic_file(magic, filename);
+	if (mimetype == NULL)
+		perror("magic_file");
+
+	free(filename);
+
 
 /* todo:
-	 parse mimetype (free it when done)
+	 parse mimetype
 	 find handlers (if any) for that mimetype
 	 execute handlers, continuing on to the next if exit code != 0
 */
+	magic_close(magic);
 }
