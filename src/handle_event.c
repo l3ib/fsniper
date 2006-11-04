64f9871f24b4a2f87f215aed3cf91d0cfc05f721
andrewy <andrewy>
2006-11-04 06:46:02 +0000

    parser works. for real.

--- src/handle_event.c
+++ src/handle_event.c
@@ -15,7 +15,7 @@ void handle_event(struct watchnode* node, struct inotify_event* event)
 	char* filename;
 	const char* mimetype;
 	magic_t magic;
-	int i;
+	int i, j;
 	struct keyval_section* child;
 
 	for (; node; node = node->next)
@@ -51,23 +51,36 @@ void handle_event(struct watchnode* node, struct inotify_event* event)
 		abort = 0;
 		foundslash = 0;
 
-		for (i=0; mimetype[i] && child->name[i] && abort == 0; i++)
+		for (i=0, j=0; mimetype[i] && child->name[j] && abort == 0; i++, j++)
 		{
-			if (foundslash == 0 && child->name[i] == '/')
-				foundslash = 1;
-
-			if (mimetype[i] != child->name[i] && child->name[i] != '*')
+			if (!mimetype[i])
 			{
 				abort = 1;
 				break;
 			}
-			else if (child->name[i] == '*' && foundslash == 0)
+			else if (!child->name[j])
 			{
-				while (child->name[i] != '/')
+				abort = 1;
+				break;
+			}
+
+			if (foundslash == 0 && child->name[j] == '/')
+				foundslash = 1;
+
+			if (mimetype[i] != child->name[j] && child->name[j] != '*')
+			{
+				abort = 1;
+				break;
+			}
+			else if (child->name[j] == '*' && foundslash == 0)
+			{
+				while (mimetype[i] != '/')
 					i++;
+				while (child->name[j] != '/')
+					j++;
 				foundslash = 1;
 			}
-			else if (child->name[i] == '*' && foundslash == 1)
+			else if (child->name[j] == '*' && foundslash == 1)
 				break;
 		}
 		if (abort == 0)
